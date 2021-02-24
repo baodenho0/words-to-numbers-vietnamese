a6d18cdddd7bb389fdd639df41b5230cc07ff375
lequochieu <lequochieu284@gmail.com>
2021-02-24 11:48:59 +0700

    updat test

--- test.php
+++ test.php
@@ -3,21 +3,21 @@
 require_once "numInWords.php";
 require_once "index.php";
 
-//$number = rand(0, 9999999999);
+$number = rand(0, 9999999999);
 
 //for ($i = 999999; $i <= 9999999; $i++) {
 ////    $i = 120000;
 //    test($i);
 //}
 
-test(2);
+test($number);
 
 function test($number)
 {
     $words = strtolower(vn_to_str(numberInVietnameseCurrency($number)));
 
     $w = new WordsToNumbers;
-    $convertNumber = $w->displayNumbers("hai tram ba muoi chin trieu chin tram bay muoi bay ngan nam tram chin muoi mot dong chan");
+    $convertNumber = $w->displayNumbers($words);
 
 //    if ($number != $convertNumber) {
         echo 'number: ' . $number;
@@ -27,6 +27,9 @@ function test($number)
         echo 'error convertNumber: ' . $convertNumber;
         echo "<br>";
         echo "<hr>";
+    if ($number == $convertNumber) {
+        echo 'OKE';
+    }
 //    }
 
 }
